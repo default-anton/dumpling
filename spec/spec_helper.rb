2e9c5d1d6d4d50616b3d348b60c963bec0539f6a
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2016-03-06 17:48:24 +0200

    Do not run simplecov on every test run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'simplecov'
 
-SimpleCov.start do
-  add_filter '/spec/'
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
 end
 
 require 'dumpling'
