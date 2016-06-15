5f944979afb02437665d6f266d1a3b466cf5f75a
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2016-06-15 22:34:26 +0300

    Bump version up to 0.4.0

--- lib/dumpling/version.rb
+++ lib/dumpling/version.rb
@@ -1,8 +1,8 @@
 module Dumpling
   module Version
     MAJOR = 0
-    MINOR = 3
-    TINY = 2
+    MINOR = 4
+    TINY = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.').freeze
